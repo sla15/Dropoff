2725942141e65dad634c61de95d19d3f872b2abb
sla15 <sla15@github.com>
2026-01-27 20:42:38 +0000

    perf: optimize production build by installing tailwind and async maps

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./index.tsx",
+        "./screens/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
